8aaa37274076a7a5d41d833d12661c860082b44e
shishir <shis2hir6410@yahoo.com>
2020-02-26 06:20:39 +0000

    file modified twice

--- myproj/src/main/java/com/raj/App.java
+++ myproj/src/main/java/com/raj/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Welcome to jenkins !" );
+        System.out.println( "Hello World Welcome to jenkins With CICD pipeline !" );
     }
 }
